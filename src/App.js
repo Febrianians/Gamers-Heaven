d32676e7f7d0edeccf1e77f1d997ef9ffa466a4b
curiousbaker <asus.zulfadli@gmail.com>
2022-05-26 13:14:10 +0700

    menambahkan page game form & game list

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import { LandingPage, RegisterPage, LoginPage } from './Pages/index';
+import {
+  LandingPage,
+  RegisterPage,
+  LoginPage,
+  GameForm,
+  GameList,
+} from "./pages/index";
 // import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route exact path='/' element={<LandingPage />} />
-        <Route path='/register' element={<RegisterPage />} />
-        <Route path='/login' element={<LoginPage />} />
+        <Route exact path="/" element={<LandingPage />} />
+        <Route exact path="/register" element={<RegisterPage />} />
+        <Route exact path="/login" element={<LoginPage />} />
+        <Route exact path="/games" element={<GameList />} />
+        <Route exact path="/games/add" element={<GameForm />} />
       </Routes>
     </>
   );
